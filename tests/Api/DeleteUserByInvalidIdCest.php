f35f90a24fc8254ecf701cc4d558be3553f63c5e
strel <strelnieks.a@gmail.com>
2025-03-02 00:19:07 +0200

    removed empty line

--- tests/Api/DeleteUserByInvalidIdCest.php
+++ tests/Api/DeleteUserByInvalidIdCest.php
@@ -14,7 +14,6 @@ header('Content-Type: application/json');
 
 final class DeleteUserByInvalidIdCest
 {
-
     public function tryToDeleteUserByInvalidId(ApiTester $I): void
     {
         $auth = base64_encode('admin:P@ssw0rd');
